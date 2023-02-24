f5b21ab308e16076f09fe9b09564d892748bcaf9
darrhey <35179225+darrhey@users.noreply.github.com>
2023-02-24 11:28:54 +0100

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,38 +1,47 @@
 #include "main.h"
 /**
- * main - runs shell program
- *
- * Return: 0 on success
+ * shell - Infinite loop that runs shell
+ * @ac: Arg count
+ * @av: args passed to shell at beginning of prog
+ * @env: Environment
+ * Return: Void
  */
-int main(void)
+void shell(int ac, char **av, char **env)
 {
-char *path = NULL, *copy = NULL, *p = NULL;
-char *fullpath = NULL, **i = NULL;
-int status = 0;
-signal(SIGINT, SIG_IGN);
-path = _getenv("PATH");
-if (path == NULL)
-return (-1);
-while (1)
-{
-i = NULL;
-prompt();
-p = _read();
-if (*p != '\0')
-{
-i = tokenize(p);
-if (i == NULL)
-{
-free(p);
-continue;
-}
-fullpath = _fullpathbuffer(i, path, copy);
-if (checkbuiltins(i, p,status) != 0)
-continue;
-status = _forkprocess(i, p, fullpath);
-}
-else
-free(p);
-}
-return (0);
+	char *line;
+	char **args;
+	int status = 1;
+	char *tmp = NULL;
+	char *er;
+	char *filename;
+	int flow;
+
+	er = "Error";
+	do {
+		prompt();
+		line = _getline();
+		args = split_line(line);
+		flow = bridge(args[0], args);
+		if (flow == 2)
+		{
+			filename = args[0];
+			args[0] = find_path(args[0], tmp, er);
+			if (args[0] == er)
+			{
+				args[0] = search_cwd(filename, er);
+				if (args[0] == filename)
+					write(1, er, 5);
+			}
+		}
+		if (args[0] != er)
+			status = execute_prog(args, line, env, flow);
+		free(line);
+		free(args);
+	} while (status);
+	if (!ac)
+		(void)ac;
+	if (!av)
+		(void)av;
+	if (!env)
+		(void)env;
 }
