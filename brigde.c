b3b3aa62c3395cdf95eb6ffd74d065ecd81f7738
darrhey <35179225+darrhey@users.noreply.github.com>
2023-02-24 11:41:47 +0100

    Update brigde.c

--- brigde.c
+++ brigde.c
@@ -7,15 +7,14 @@
  */
 int bridge(char *check, char **args)
 {
-	int i = 0;
-
-	if (builtins_checker(args) == 1)
-		return (1);
-	while (check[i] != '\0')
-	{
-		if (check[i] == '/')
-			return (1);
-		i++;
-	}
-	return (2);
+int i = 0;
+if (builtins_checker(args) == 1)
+return (1);
+while (check[i] != '\0')
+{
+if (check[i] == '/')
+return (1);
+i++;
+}
+return (2);
 }
