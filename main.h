6c2074e9cd274749e6ff58c54b6e7bb3088d6af6
darrhey <35179225+darrhey@users.noreply.github.com>
2023-02-24 11:25:23 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,39 +1,46 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef  _MAIN_H_
+#define  _MAIN_H_
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <stddef.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <signal.h>
-#include <fcntl.h>
-
-extern char **env;
-
-int prompt(void);
-char *_read(void);
-char *_fullpathbuffer(char **i, char *path, char *copy);
-int checkbuiltins(char **i, char *p, int status);
-int _forkprocess(char **i, char *p, char *fullpath);
-
-char *_strdup(char *str);
-int _splitstring(char *str);
-int _strcmp(const char *s1, const char *s2);
-int _strcat(char *dest, char *src);
+#define TOKENS_BUFFER_SIZE 64
+#define LINE_SIZE 1024
+#define TOKEN_DELIMITERS " \t\r\n\a"
+extern char **environ;
+/**
+ * struct builtins - Has builtins and associated funcs
+ * @arg: Builtins name
+ * @builtin: Mathcing builtin func
+ */
+typedef struct builtins
+{
+	char *arg;
+	void (*builtin)(char **args, char *line, char **env);
+} builtins_t;
+void shell(int ac, char **av, char **env);
+char *_getline(void);
+char **split_line(char *line);
+int execute_prog(char **args, char *line, char **env, int flow);
+int check_for_builtins(char **args, char *line, char **env);
+int launch_prog(char **args);
+void exit_shell(char **args, char *line, char **env);
+void env_shell(char **args, char *line, char **env);
+int _strcmp(char *s1, char *s2);
+char *find_path(char *args, char *tmp, char *er);
+char *search_cwd(char *filename, char *er);
+int bridge(char *check, char **args);
+void prompt(void);
+int builtins_checker(char **args);
+char *save_path(char *tmp, char *path);
+char *read_dir(char *er, struct dirent *s, char *fi, int l, char *p, char *t);
+char *_getenv(char *env);
+char *_strstr(char *haystack, char *needle);
 int _strlen(char *s);
-
-char **tokenize(char *p);
-int _splitPATH(char *str);
-int _PATHstrcmp(const charc *s1, const char *s2);
-char *_concat(char *tmp, cahr **i, char *tok);
-
-char *_getenv(const char *name);
-int _env(void);
-void _puts(char *str);
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-
-#endif /*MAIN_H*/
+#endif /*MAIN*/
